efd927dc4edecd451c36c86d94d44db9e06140d4
santun <santun1993@gmail.com>
2020-12-21 21:52:57 +0630

    Update Logo

---
+++ lib/ui/screen/home/home_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inject/inject.dart';
+import 'package:masterstudy_app/ui/bloc/home/bloc.dart';
+import 'package:masterstudy_app/ui/widgets/loading_error_widget.dart';
+
+import '../../../theme/theme.dart';
+import 'items/categories_item.dart';
+import 'items/new_courses_item.dart';
+import 'items/top_instructors.dart';
+import 'items/trending_item.dart';
+
+@provide
+class HomeScreen extends StatefulWidget {
+  const HomeScreen() : super();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  HomeBloc _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = BlocProvider.of<HomeBloc>(context)..add(FetchEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(
+        //     backgroundColor: HexColor.fromHex("#191970"),
+        //     centerTitle: true,
+        //     title: Text(
+        //       "HOME",
+        //       textScaleFactor: 1.0,
+        //       style: TextStyle(fontSize: 16.0, color: Colors.white),
+        //     )),
+        // appBar: PreferredSize(
+        //   preferredSize: Size.fromHeight(0),
+        //   child: AppBar(
+        //     brightness: Brightness.dark,
+        //   ),
+        // ),
+        body: BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        return _buildBody(context, state);
+      },
+    ));
+  }
+
+  _buildBody(context, state) {
+    if (state is LoadedHomeState) {
+      return ListView.builder(
+          itemCount: state.layout.length,
+          itemBuilder: (context, index) {
+            var item = state.layout[index];
+            switch (item.id) {
+              // case 3:
+              //   return TrendingWidget(true, item.name, state.coursesTranding);
+              // case 4:
+              //   return TopInstructorsWidget(item.name, state.instructors);
+              case 1:
+                return CategoriesWidget(item.name, state.categoryList);
+              case 2:
+                return NewCoursesWidget(item.name, state.coursesNew);
+              // case 5:
+              //   return TrendingWidget(false, item.name, state.coursesFree);
+              default:
+              // return CategoriesWidget(item.name, state.categoryList);
+              // return NewCoursesWidget(item.name, state.coursesNew);
+            }
+          });
+    }
+    if (state is InitialHomeState) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (state is ErrorHomeState) {
+      return LoadingErrorWidget(() {
+        _bloc.add(FetchEvent());
+      });
+    }
+  }
+}
---
+++ lib/ui/screen/main/main_screen.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:masterstudy_app/data/models/AppSettings.dart';
+import 'package:masterstudy_app/main.dart';
+import 'package:masterstudy_app/ui/screen/courses/courses_screen.dart';
+import 'package:masterstudy_app/ui/screen/favorites/favorites_screen.dart';
+import 'package:masterstudy_app/ui/screen/home/home_screen.dart';
+import 'package:masterstudy_app/ui/screen/home_simple/home_simple_screen.dart';
+import 'package:masterstudy_app/ui/screen/profile/profile_screen.dart';
+import 'package:masterstudy_app/ui/screen/search/search_screen.dart';
+
+import '../../../theme/theme.dart';
+
+class MainScreenArgs {
+  final OptionsBean optionsBean;
+
+  MainScreenArgs(this.optionsBean);
+}
+
+class MainScreen extends StatelessWidget {
+  static const routeName = "mainScreen";
+
+  const MainScreen() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    final MainScreenArgs args = ModalRoute.of(context).settings.arguments;
+
+    return MainScreenWidget(args.optionsBean);
+  }
+}
+
+class MainScreenWidget extends StatefulWidget {
+  final OptionsBean optionsBean;
+
+  const MainScreenWidget(this.optionsBean) : super();
+
+  @override
+  State<StatefulWidget> createState() {
+    return MainScreenState();
+  }
+}
+
+class MainScreenState extends State<MainScreenWidget> {
+  String token;
+  var _selectedIndex = 0;
+  final _selectedItemColor = Color(0xFF191970);
+  final _selectedBgColor = Colors.white;
+  final _unselectedItemColor = Colors.white;
+  final _unselectedBgColor = Color(0xFF191970);
+  final Widget search = SvgPicture.asset('assets/icons/search.svg');
+
+  Color _getBgColor(int index) =>
+      _selectedIndex == index ? _selectedBgColor : _unselectedBgColor;
+
+  Color _getItemColor(int index) =>
+      _selectedIndex == index ? _selectedItemColor : _unselectedItemColor;
+
+  Widget _buildIcon(String iconData, String text, int index) => Container(
+        width: double.infinity,
+        height: kBottomNavigationBarHeight,
+        child: Material(
+          color: _getBgColor(index),
+          child: InkWell(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //Icon(iconData),
+                Padding(
+                  padding: EdgeInsets.only(top: 2.0, bottom: 4.0),
+                  child: SvgPicture.asset(iconData,
+                      height: 22.0, color: _getItemColor(index)),
+                ),
+                Text(text,
+                    textScaleFactor: 1.0,
+                    style:
+                        TextStyle(fontSize: 12, color: _getItemColor(index))),
+              ],
+            ),
+            onTap: () => _onItemTapped(index),
+          ),
+        ),
+      );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: HexColor.fromHex("#191970"),
+        centerTitle: true,
+        title: SvgPicture.asset('assets/icons/MPS-Logo.svg'),
+        actions: [IconButton(icon: search, onPressed: null)],
+      ),
+      drawer: Drawer(
+        child: LayoutBuilder(
+          builder: (context, constraint) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraint.maxHeight),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: <Widget>[
+                      new UserAccountsDrawerHeader(
+                          margin: EdgeInsets.zero,
+                          // padding: EdgeInsets.zero,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    "assets/images/drawer_header.jpg")),
+                          ),
+                          accountEmail: Text(''),
+                          accountName: Row(
+                            children: <Widget>[
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: ShapeDecoration(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(50),
+                                    ),
+                                  ),
+                                  image: DecorationImage(
+                                      image:
+                                          AssetImage("assets/images/user.png")),
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'Maxart Olivia',
+                                    style: new TextStyle(
+                                      color: Colors.blueGrey[50],
+                                      // textBaseline: TextAlign.left,
+                                    ),
+                                  ),
+                                  Text(
+                                    '+95 9 123 456 789',
+                                    style: new TextStyle(
+                                        color: Colors.blueGrey[50]),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                      new ListTile(
+                        title: new Text('ပင်မစာမျက်နှာ'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 1;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        title: new Text('အပ်နှံထားသောအတန်းများ'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 0;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        title: new Text('ကျွန်ုပ်အကြောင်း'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 0;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        title: new Text('ဆက်သွယ်ရန်'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 0;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        title: new Text('ပြင်ဆင်ရန်'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 0;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        title: new Text('ဝေမျှမည်'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 0;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        title: new Text('ထွက်ခွာရန်'),
+                        onTap: () {
+                          this.setState(() {
+                            // screen = 0;
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new Divider(),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      body: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 150),
+          child: _getBody(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: _buildIcon("assets/icons/tab_home.svg", "ပင်မစာမျက်နှာ", 0),
+              title: SizedBox.shrink()),
+          BottomNavigationBarItem(
+              icon: _buildIcon("assets/icons/tab_book.svg", "သင်ခန်းစာများ", 1),
+              title: SizedBox.shrink()),
+          BottomNavigationBarItem(
+              icon: _buildIcon("assets/icons/tab_blog.svg", "ဆောင်းပါးများ", 2),
+              title: SizedBox.shrink()),
+          // BottomNavigationBarItem(
+          //     icon: _buildIcon("assets/icons/ms_nav_fav.svg",
+          //         localizations.getLocalization("favorites_bottom_nav"), 3),
+          //     title: SizedBox.shrink()),
+          // BottomNavigationBarItem(
+          //     icon: _buildIcon("assets/icons/ms_nav_profile.svg",
+          //         localizations.getLocalization("profile_bottom_nav"), 4),
+          //     title: SizedBox.shrink()),
+        ],
+        selectedFontSize: 0,
+        currentIndex: _selectedIndex,
+        selectedItemColor: _selectedItemColor,
+        unselectedItemColor: _unselectedItemColor,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void _onItemTapped(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
+  }
+
+  Widget _getBody(int index) {
+    switch (index) {
+      case 0:
+        return (widget.optionsBean?.app_view ?? true)
+            ? HomeSimpleScreen()
+            : HomeScreen();
+      case 1:
+        return CoursesScreen(() {
+          setState(() {
+            _selectedIndex = 0;
+          });
+        });
+      case 2:
+        return SearchScreen();
+      case 3:
+        return FavoritesScreen();
+      case 4:
+        return ProfileScreen(() {
+          setState(() {
+            _selectedIndex = 1;
+          });
+        });
+      default:
+        return Center(
+          child: Text(
+            "Not implemented!",
+            textScaleFactor: 1.0,
+          ),
+        );
+    }
+  }
+}
